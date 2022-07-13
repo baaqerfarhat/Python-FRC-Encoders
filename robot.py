1545943e4fe0a895c3912c6b6d81cb61e1b081b9
baaqerfarhat <53981711+baaqerfarhat@users.noreply.github.com>
2022-07-13 09:02:22 -0500

    adding auton

--- robot.py
+++ robot.py
@@ -1,7 +1,11 @@
 
-from ctre import TalonSRX
+
+from time import sleep
 import wpilib
 from wpilib.drive import MecanumDrive
+import ctre
+import math
+
 
 class MyRobot(wpilib.TimedRobot):
 
@@ -11,15 +15,14 @@ class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         """Robot initialization function"""
 
-        self.frontLeftMotor = TalonSRX(4)
-        self.rearLeftMotor = TalonSRX(3)
-        self.frontRightMotor = TalonSRX(1)
-        self.rearRightMotor = TalonSRX(2)
+        self.frontLeftMotor = ctre.WPI_TalonSRX(4)
+        self.rearLeftMotor = ctre.WPI_TalonSRX(3)
+        self.frontRightMotor = ctre.WPI_TalonSRX(1)
+        self.rearRightMotor = ctre.WPI_TalonSRX(2)
+        
 
         # invert the left side motors
         self.frontLeftMotor.setInverted(True)
-
-        # you may need to change or remove this to match your robot
         self.rearLeftMotor.setInverted(True)
 
         self.drive = MecanumDrive(
@@ -36,13 +39,34 @@ class MyRobot(wpilib.TimedRobot):
     def teleopInit(self):
         self.drive.setSafetyEnabled(True)
 
+    
+
     def teleopPeriodic(self):
         """Runs the motors with Mecanum drive."""
         # Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         # This sample does not use field-oriented drive, so the gyro input is set to zero.
         self.drive.driveCartesian(
-            self.stick.getX(), self.stick.getY(), self.stick.getZ(), 0
+            self.stick.getY(), self.stick.getX(), self.stick.getZ(),
         )
+
+    
+    
+def autonomousInit(self):
+        frontLeftMotor.GetSelectedSensorpos()
+    
+    
+def autonomousPeriodic(self): 
+
+            wpilib.Encoder()
+            self.frontLeftMotor.set(1)
+            self.frontRightMotor.set(1)
+            self.rearLeftMotor.set(1)
+            self.rearRightMotor.set(1)
+            sleep()
+
+    
+
+
 
 
 if __name__ == "__main__":
